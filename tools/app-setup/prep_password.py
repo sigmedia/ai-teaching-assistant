e0911995c8f15e5fd56a7ca9bb514c2a80d72688
Deirdre O'Regan <dregan@tcd.ie>
2025-07-13 12:40:12 +0100

    Modifying password prep too to use command line

--- tools/app-setup/prep_password.py
+++ tools/app-setup/prep_password.py
@@ -1,9 +1,18 @@
+
 import bcrypt
+import argparse
 
 def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 def main():
-    print(hash_password("myPassword"))
+    parser = argparse.ArgumentParser(description='Hash a password using bcrypt')
+    parser.add_argument('password', help='Password to hash')
     
-main()
+    args = parser.parse_args()
+    
+    hashed = hash_password(args.password)
+    print(hashed.decode('utf-8'))
+
+if __name__ == "__main__":
+    main()
