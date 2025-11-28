ecfbd3b7b880f72d474521e479490d617dd1e591
Dongjun Geng <gengd@tcd.ie>
2025-11-28 16:47:50 +0000

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, JSON, ForeignKey, Index, update
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Index, update
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from sqlalchemy import event
+from sqlalchemy.types import Unicode, UnicodeText
 
 Base = declarative_base()
 
@@ -15,9 +16,9 @@ class Session(Base):
     SessionID = Column(String(64), unique=True, nullable=False)
     Active = Column(Boolean(1), default=True, nullable=False)
     HasAgreedToPart1 = Column(Boolean(1), default=False, nullable=False)
-    AgreementPart1Version = Column(String(64), nullable=False)
+    AgreementPart1Version = Column(Unicode(64), nullable=False)
     HasAgreedToPart2 = Column(Boolean(1), default=False, nullable=False)
-    AgreementPart2Version = Column(String(64), nullable=False)
+    AgreementPart2Version = Column(Unicode(64), nullable=False)
     DateAgreedToPart1 = Column(DateTime(timezone=True), server_default=func.GETUTCDATE(), nullable=False)
     DateAgreedToPart2 = Column(DateTime(timezone=True), server_default=func.GETUTCDATE(), nullable=False)
     DateCreated = Column(DateTime(timezone=True), server_default=func.GETUTCDATE(), nullable=False)
@@ -32,7 +33,7 @@ class Message(Base):
     Id = Column(Integer, primary_key=True, autoincrement=True)
     SessionID = Column(String(64), ForeignKey('sessions.SessionID'), nullable=False)
     IsBot = Column(Boolean(1), nullable=False)
-    MessageText = Column(Text(), nullable=True)
+    MessageText = Column(UnicodeText(), nullable=True)
     DateCreated = Column(DateTime(timezone=True), server_default=func.GETUTCDATE(), nullable=False)
     session = relationship("Session", back_populates="messages")
 
